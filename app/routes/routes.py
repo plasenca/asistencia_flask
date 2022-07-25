5d70432bc7237d6d4b2d338e53986f58c683f76e
Franzua Plasencia <franzua.15pc@gmail.com>
2022-07-25 17:50:19 -0500

    [Flask Asistencia / Dashboard]

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -2,9 +2,17 @@ from flask import render_template, redirect
 from app import app
 
 @app.route('/')
-def main():
+def index():
     return redirect("login"), 302
 
 @app.route('/login')
 def login():
     return render_template('login/login.html'), 200
+
+@app.route('/main')
+def main():
+    return render_template('main/main.html'), 200
+
+@app.route("/test")
+def test():
+    return render_template('main/asistencia.html'), 200
